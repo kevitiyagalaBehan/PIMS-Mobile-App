2051e4aedac2e5a09ab19806dc57009413e756a5
Behan Kevitiyagala <beyhan.k@boroncorp.org>
2025-03-01 11:06:14 +0530

    Created Home Screen

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Portfolio App</Text>
+      <Text>Select a tab to view data</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  title: { fontSize: 22, fontWeight: "bold" },
+});
