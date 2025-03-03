3b339bf7ac623954c536d9e5b6eb04628b0fd6a6
Behan Kevitiyagala <beyhan.k@boroncorp.org>
2025-03-03 22:50:52 +0530

    Updated

--- frontend/App.js
+++ frontend/App.js
@@ -1,27 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { AuthProvider, useAuth } from "./src/context/AuthContext";
+import ChartScreen from "./src/screens/ChartScreen";
+import TableScreen from "./src/screens/TableScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import HomeScreen from "./src/screens/HomeScreen";
 
 const Stack = createStackNavigator();
 
-export default function App() {
-  const [userData, setUserData] = useState(null);
+function AppNavigator() {
+  const { userData } = useAuth();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {userData ? (
-          <Stack.Screen name="Home">
-            {() => <HomeScreen userData={userData} />}
-          </Stack.Screen>
+      <Stack.Navigator initialRouteName={userData ? "Home" : "Login"}>
+        {!userData ? (
+          <Stack.Screen name="Login" component={LoginScreen} />
         ) : (
-          <Stack.Screen name="Login">
-            {() => <LoginScreen setUserData={setUserData} />}
-          </Stack.Screen>
+          <>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="DoughnutChart" component={ChartScreen} />
+            <Stack.Screen name="Table" component={TableScreen} />
+          </>
         )}
       </Stack.Navigator>
     </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <AppNavigator />
+    </AuthProvider>
   );
 }
