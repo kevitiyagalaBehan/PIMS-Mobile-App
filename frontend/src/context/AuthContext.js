e353c0bf049e2670a5a3a6290668d413d9cd7214
Behan Kevitiyagala <beyhan.k@boroncorp.org>
2025-03-03 22:53:07 +0530

    React context

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState, useContext } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [userData, setUserData] = useState(null);
+
+  return (
+    <AuthContext.Provider value={{ userData, setUserData }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
