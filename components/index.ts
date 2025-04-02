c000b23d92c85598e11048bd5b735caff9d79c94
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-04-02 12:19:16 +0530

    Created index file to access multiple components

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header";
+export { default as PortfolioSummary } from "./PortfolioSummaryTable";
+export { default as AssetAllocation } from "./AssetAllocationPieChart";
+export { default as BackButton } from "./BackButton";
