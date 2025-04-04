6837d3991502d9f6ae0f2b1c9f95ef6225939fcb
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-04-04 14:50:51 +0530

    Updated drawer as root navigator

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { AuthProvider, useAuth } from "./src/context/AuthContext";
 import LoginScreen from "./src/screens/LoginScreen";
 import DrawerNavigator from "./src/navigation/DrawerNavigator";
+import BottomTab from "./src/navigation/BottomTabNavigator";
 
 const Stack = createStackNavigator();
 
@@ -12,14 +13,14 @@ function AppNavigator() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={userData ? "Home" : "Login"}>
+      <Stack.Navigator initialRouteName={userData ? "Main" : "Login"}>
         <Stack.Screen
           name="Login"
           component={LoginScreen}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name="Home"
+          name="Main"
           component={DrawerNavigator}
           options={{ headerShown: false }}
         />
