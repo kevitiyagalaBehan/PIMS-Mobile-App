6b0d776b4228849d299ebf13282878fba728d820
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-05-15 16:16:33 +0530

    Updated

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,6 @@
 export { default as Header } from "./Header";
 export { default as PortfolioSummary } from "./PortfolioSummary";
 export { default as AssetAllocation } from "./AssetAllocation";
-export { default as BackButton } from "./BackButton";
 export { default as PortfolioBalanceSummary } from "./PortfolioBalanceSummary";
 export { default as TopTenInvestments } from "./TopTenInvestments";
 export { default as ContributionCapSummary } from "./ContributionCapSummary";
