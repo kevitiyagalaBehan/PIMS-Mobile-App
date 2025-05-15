5e9ae464995f0c37adb767f4887604b7332e3da8
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-05-15 15:06:58 +0530

    Updated as needed to auto logout

--- src/utils/logout.ts
+++ src/utils/logout.ts
@@ -1,17 +1,28 @@
 import { NavigationProp } from "@react-navigation/native";
 import { Alert } from "react-native";
 
-export const handleLogout = (navigation: NavigationProp<any>) => {
-  Alert.alert("Logout", "Are you sure you want to log out?", [
-    { text: "Cancel", style: "cancel" },
-    {
-      text: "Logout",
-      onPress: () => {
-        navigation.reset({
-          index: 0,
-          routes: [{ name: "Login" }],
-        });
+export const handleLogout = (
+  navigation: NavigationProp<any>,
+  showConfirmation: boolean = true
+) => {
+  if (showConfirmation) {
+    Alert.alert("Logout", "Are you sure you want to log out?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Logout",
+        onPress: () => {
+          performLogout(navigation);
+        },
       },
-    },
-  ]);
+    ]);
+  } else {
+    performLogout(navigation);
+  }
+};
+
+const performLogout = (navigation: NavigationProp<any>) => {
+  navigation.reset({
+    index: 0,
+    routes: [{ name: "Login" }],
+  });
 };
