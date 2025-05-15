53ed241dea74294587997935a312cb8fa02de2c1
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-05-15 15:10:58 +0530

    Removed React library

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { StatusBar } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
