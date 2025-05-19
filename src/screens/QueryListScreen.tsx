bdab4fd87c5a96a0f3af68f2527830a4e0b954f6
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-05-19 15:43:33 +0530

    Created Query Screen

---
+++ src/screens/QueryListScreen.tsx
@@ -0,0 +1,25 @@
+import { Text, StyleSheet } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function QueryList() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.bodytext}>Under Maintenance</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bodytext: {
+    color: "#FF0000",
+    fontSize: 25,
+    fontWeight: "bold",
+  }
+})
