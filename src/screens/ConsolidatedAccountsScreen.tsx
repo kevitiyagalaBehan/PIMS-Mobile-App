898571191ff97e5c2db727e9192624d7409dc1f4
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-05-23 18:00:16 +0530

    Created consolidated screen for family groups

---
+++ src/screens/ConsolidatedAccountsScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ConsolidatedAccountsScreen() {
+  return (
+    <View>
+      <Text>ConsolidatedAccountsScreen</Text>
+    </View>
+  )
+}
