26af1c7da741bdc7b144d14f4928ee2c25130181
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-05-30 11:11:12 +0530

    Created new screen

--- src/screens/ConsolidatedAccountsScreen.tsx
+++ src/screens/ConsolidatedAccountsScreen.tsx
@@ -1,10 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { StyleSheet, RefreshControl, ScrollView, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ConsolidatedAccounts, HeaderWithMenu } from "../../components";
+import { useRefreshTrigger } from "../../hooks/useRefreshTrigger";
+import { useWindowSize } from "../../hooks/useWindowSize";
 
-export default function ConsolidatedAccountsScreen() {
+export default function TopTenInvestmentsScreen() {
+  const { width } = useWindowSize();
+  const { refreshTrigger, refreshing, onRefresh } = useRefreshTrigger();
+
+  const styles = getStyles(width);
+
   return (
-    <View>
-      <Text>ConsolidatedAccountsScreen</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <HeaderWithMenu />
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        <View style={styles.bodySection}>
+          <ConsolidatedAccounts
+            refreshTrigger={refreshTrigger}
+            refreshing={refreshing}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+
+const getStyles = (width: number) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    bodySection: {
+      paddingHorizontal: width * 0.02,
+      backgroundColor: "#eee",
+    },
+  });
