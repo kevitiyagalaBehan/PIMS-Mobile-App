ede9ea2849c7fbec9b9917efef7f8f2053708dbe
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-05-30 11:10:34 +0530

    Updated

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "PIMS",
     slug: "PIMS",
-    version: "1.0.4",
+    version: "1.0.5",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
