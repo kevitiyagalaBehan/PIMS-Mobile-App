87ce6b2ed80365c5029107ce3cdd805c0d77a0d1
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-06-12 18:08:59 +0530

    Created portfolio screen

---
+++ src/screens/PortfolioScreen.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  StyleSheet,
+  RefreshControl,
+  ScrollView,
+  View,
+  useWindowDimensions,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { HeaderWithMenu, Portfolio } from "../../components";
+import { useRefreshTrigger } from "../../hooks/useRefreshTrigger";
+
+export default function PortfolioScreen() {
+  const { width, height } = useWindowDimensions();
+  const { refreshTrigger, refreshing, onRefresh } = useRefreshTrigger();
+
+  const styles = getStyles(width);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <HeaderWithMenu />
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+        contentContainerStyle={{
+          flexGrow: 1,
+          paddingBottom: height * 0.01,
+        }}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.bodySection}>
+          <Portfolio
+            refreshTrigger={refreshTrigger}
+            refreshing={refreshing}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const getStyles = (width: number) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    bodySection: {
+      paddingHorizontal: width * 0.02,
+      backgroundColor: "#eee",
+    },
+  });
