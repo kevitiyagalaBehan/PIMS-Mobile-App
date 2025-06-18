f60cd02ea8f3d702430b60ab1573ecb701cc9c0c
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-06-18 15:20:19 +0530

    Updated

--- src/screens/DocumentScreen.tsx
+++ src/screens/DocumentScreen.tsx
@@ -1,22 +1,33 @@
-import { StyleSheet, Text, SafeAreaView, useWindowDimensions } from 'react-native'
-import React from 'react'
-import { HeaderWithMenu } from '../../components'
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  useWindowDimensions,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Document, HeaderWithMenu } from "../../components";
+import { useRefreshTrigger } from "../../hooks/useRefreshTrigger";
 
 export default function DocumentScreen() {
-const { width, height } = useWindowDimensions();
+  const { width, height } = useWindowDimensions();
   const styles = getStyles(width);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <HeaderWithMenu />
-      <Text>Under Maintenance</Text>
-    </SafeAreaView>
-  )
-}
-
-const getStyles = (width: number) =>
-  StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-  });
+      <SafeAreaView style={styles.container}>
+        <HeaderWithMenu />
+          <View style={styles.bodySection}>
+            <Document />
+          </View>
+      </SafeAreaView>
+    );
+  }
+  
+  const getStyles = (width: number) =>
+    StyleSheet.create({
+      container: {
+        flex: 1,
+      },
+      bodySection: {
+        paddingHorizontal: width * 0.02,
+      },
+    });
