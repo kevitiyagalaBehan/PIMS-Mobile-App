7862df409a844b90fc01ad0072e0b49a74a96fc2
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-06-24 11:54:08 +0530

    Updated

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "PIMS",
     slug: "PIMS",
-    version: "1.0.9",
+    version: "1.0.10",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -13,7 +13,7 @@ export default {
       supportsTablet: true,
     },
     android: {
-      versionCode: 27,
+      versionCode: 28,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff",
